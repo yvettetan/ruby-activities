21586f3416ac06320a4b9e9a782ffe42585c34bd
Yvette Tan <yvettetan97@gmail.com>
2021-06-29 23:10:03 +0800

    updated

--- square_of_squares.rb
+++ square_of_squares.rb
@@ -1,12 +1,20 @@
-puts "Enter a number to check if it\'s a perfect square"
-number = gets.chomp.to_i
-if number.negative?
-  puts "#{number} is a not perfect square"
-else
+# puts "Enter a number to check if it\'s a perfect square"
+# number = gets.chomp.to_i
+
+def perfect_square(number)
   square_root = number**0.5
-  if square_root * square_root == number
+  if number.negative?
+    puts "#{number} is a not perfect square"
+  elsif number.zero? || square_root.modulo(1).zero?
     puts "#{number} is a perfect square"
   else
     puts "#{number} is not a perfect square"
   end
 end
+
+perfect_square(-1)
+perfect_square(0)
+perfect_square(3)
+perfect_square(4)
+perfect_square(25)
+perfect_square(26)
