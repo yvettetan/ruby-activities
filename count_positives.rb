5d41d676b6ff5b4ec04511f0b2c90238b50bee0d
Yvette Tan <yvettetan97@gmail.com>
2021-06-29 20:00:06 +0800

    answered

---
+++ count_positives.rb
@@ -0,0 +1,14 @@
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]
+
+if arr.length.zero?
+  puts []
+else
+  positive_arr = arr.find_all { |num| num > 0 }
+  negative_arr = arr.find_all { |num| num < 0 }
+  sum_neg = 0
+  negative_arr.each do |num|
+    sum_neg += num
+  end
+  count_arr = [positive_arr.length, sum_neg]
+  p count_arr
+end
