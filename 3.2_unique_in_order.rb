59f3b34728fcaadb9652d2cac2d26d0999277ea5
Yvette Tan <yvettetan97@gmail.com>
2021-07-08 14:39:42 +0800

    Refactors code

--- 3.2_unique_in_order.rb
+++ 3.2_unique_in_order.rb
@@ -2,19 +2,10 @@
 # with the same value next to each other and preserving the original order of elements.
 
 def unique_in_order(sequence)
-  i = 0
-  y = i + 1
-  while i < sequence.length
-    if sequence[i] == sequence[y]
-      sequence.is_a?(String) ? sequence.squeeze!(sequence[y]) : sequence.delete_at(y)
-    end
-    i += 1
-    y += 1
-  end
-  p sequence.split('') if sequence.is_a?(String)
-  p sequence if sequence.is_a?(Array)
+  p sequence.chunk(&:itself).map(&:first) if sequence.is_a?(Array)
+  p sequence.squeeze!.split('') if sequence.is_a?(String)
 end
 
 unique_in_order('AAAAABBBCCDAABBB') # ["A", "B", "C", "D", "A", "B"]
 unique_in_order('ABBCcAD') # ["A", "B", "C", "c", "A", "D"]
-unique_in_order([1, 2, 2, 3, 3]) # [1,2,3]
+unique_in_order([1, 2, 2, 3, 3]) # [1, 2, 3]
